db98fc864341951fb1112663a6e545b595cdda01
Hanying Jiang <hjiang252@wisc.edu>
2023-07-18 14:26:51 -0500

    reorganize codes

--- R/data_simulator.R
+++ R/data_simulator.R
@@ -1,7 +1,4 @@
-#=================================
-# Generate simulation data: causally dependent
-sim.data_dep<-function(n,Z,a,b,c,Delta,Xi1,Sigma2)
-{
+sim.data_dep<-function(n,Z,a,b,c,Delta,Xi1,Sigma2) {
   k<-nrow(b)
   
   M.coef<-rbind(a,Delta)
@@ -9,14 +6,11 @@ sim.data_dep<-function(n,Z,a,b,c,Delta,Xi1,Sigma2)
   
   eps1<-matrix(NA,n,k)
   M<-matrix(NA,n,k)
-  for(j in 1:k)
-  {
+  for(j in 1:k) {
     eps1[,j]<-rnorm(n,sd=sqrt(Xi1[j,j]))
-    if(j==1)
-    {
+    if(j==1) {
       M[,j]<-Z*A[1,1]+eps1[,j]
-    }else
-    {
+    } else {
       M[,j]<-cbind(Z,M[,1:(j-1)])%*%M.coef[1:j,j]+eps1[,j]
     }
   }
@@ -25,7 +19,3 @@ sim.data_dep<-function(n,Z,a,b,c,Delta,Xi1,Sigma2)
   
   return(list(M=M,R=R))
 }
-#=================================
-
-
-
--- R/fit.R
+++ R/fit.R
@@ -1,11 +1,4 @@
-#################################################################
-# Adaptive pathway lasso functions
-#################################################################
-
-#################################################################
-# log-likelihood function
-mediation_net_ll<-function(Z,M,R,A,B,C,Sigma)
-{
+mediation_net_ll<-function(Z,M,R,A,B,C,Sigma) {
   n<-nrow(M)
   k<-ncol(M)
   
@@ -14,11 +7,9 @@ mediation_net_ll<-function(Z,M,R,A,B,C,Sigma)
   
   l1<--n*sum(log(diag(Sigma1)))/2
   l2<--n*log(Sigma2[1,1])/2
-  if(k==1)
-  {
+  if(k==1) {
     l3<--(t(M-Z%*%A)%*%(M-Z%*%A))[1,1]/(2*Sigma1[1,1])
-  }else
-  {
+  } else {
     l3<--sum(diag(diag(1/diag(Sigma1))%*%t(M-Z%*%A)%*%(M-Z%*%A)))/2
   }
   l4<--(t(R-Z%*%C-M%*%B)%*%(R-Z%*%C-M%*%B))[1,1]/(2*Sigma2[1,1])
@@ -33,94 +24,60 @@ mediation_net_ll<-function(Z,M,R,A,B,C,Sigma)
   
   return(re)
 }
-#################################################################
 
-#################################################################
-# solution function
-
-# soft-thresholding function
-soft_thred<-function(mu,lambda)
-{
+soft_thred<-function(mu,lambda) {
   return(max(abs(mu)-lambda,0)*sign(mu))
 }
 
+
 # different omega's
-solution<-function(lambda,omega1=0,omega2=0,phi1,phi2,mu1,mu2)
-{
-  if(lambda==0)
-  {
+solution<-function(lambda,omega1=0,omega2=0,phi1,phi2,mu1,mu2) {
+  if(lambda==0) {
     a<-soft_thred(mu1,omega1)/phi1
     b<-soft_thred(mu2,omega2)/phi2
     
     return(data.frame(a=a,b=b))
-  }else
-  {
-    de<-phi1*phi2-lambda^2
-    
-    x1<-phi2*(mu1-omega1)-lambda*(mu2-omega2)
-    x2<-phi2*(mu1-omega1)+lambda*(mu2+omega2)
-    x3<-phi2*(mu1+omega1)+lambda*(mu2-omega2)
-    x4<-phi2*(mu1+omega1)-lambda*(mu2+omega2)
-    
-    y1<-phi1*(mu2-omega2)-lambda*(mu1-omega1)
-    y2<-phi1*(mu2+omega2)+lambda*(mu1-omega1)
-    y3<-phi1*(mu2-omega2)+lambda*(mu1+omega1)
-    y4<-phi1*(mu2+omega2)-lambda*(mu1+omega1)
-    
-    s1<-data.frame(a=x1/de,b=y1/de)
-    s2<-data.frame(a=x2/de,b=y2/de)
-    s3<-data.frame(a=x3/de,b=y3/de)
-    s4<-data.frame(a=x4/de,b=y4/de)
-    s5<-data.frame(a=soft_thred(mu1,omega1)/phi1,b=0)
-    s6<-data.frame(a=0,b=soft_thred(mu2,omega2)/phi2)
-    s7<-data.frame(a=0,b=0)
-    
-    if(x1>0&y1>0)
-    {
-      return(s1)
-    }else
-      if(x2>0&y2<0)
-      {
-        return(s2)
-      }else
-        if(x3<0&y3>0)
-        {
-          return(s3)
-        }else
-          if(x4<0&y4<0)
-          {
-            return(s4)
-          }else
-            if(abs(mu1)>omega1&((phi1*abs(mu2)-lambda*abs(mu1))<=(-lambda*omega1+phi1*omega2)))
-            {
-              return(s5)
-            }else
-              if(abs(mu2)>omega2&((phi2*abs(mu1)-lambda*abs(mu2))<=(-lambda*omega2+phi2*omega1)))
-              {
-                return(s6)
-              }else
-              {
-                return(s7)
-              }
   }
+  
+  de<-phi1*phi2-lambda^2
+  
+  x1<-phi2*(mu1-omega1)-lambda*(mu2-omega2)
+  y1<-phi1*(mu2-omega2)-lambda*(mu1-omega1)
+  if(x1>0&y1>0) { return(data.frame(a=x1/de,b=y1/de)) }
+  
+  x2<-phi2*(mu1-omega1)+lambda*(mu2+omega2)
+  y2<-phi1*(mu2+omega2)+lambda*(mu1-omega1)
+  if(x2>0&y2<0) { return(data.frame(a=x2/de,b=y2/de)) }
+  
+  x3<-phi2*(mu1+omega1)+lambda*(mu2-omega2)
+  y3<-phi1*(mu2-omega2)+lambda*(mu1+omega1)
+  if(x3<0&y3>0) { return(data.frame(a=x3/de,b=y3/de)) }
+    
+  x4<-phi2*(mu1+omega1)-lambda*(mu2+omega2)
+  y4<-phi1*(mu2+omega2)-lambda*(mu1+omega1)
+  if(x4<0&y4<0) { return(data.frame(a=x4/de,b=y4/de)) }
+  
+  if (abs(mu1)>omega1&((phi1*abs(mu2)-lambda*abs(mu1))<=(-lambda*omega1+phi1*omega2))) {
+    return(data.frame(a=soft_thred(mu1,omega1)/phi1,b=0))
+  }
+  
+  if (abs(mu2)>omega2&((phi2*abs(mu1)-lambda*abs(mu2))<=(-lambda*omega2+phi2*omega1))) {
+    return(data.frame(a=0,b=soft_thred(mu2,omega2)/phi2))
+  }
+  
+  return(data.frame(a=0,b=0))
 }
-#################################################################
 
-#################################################################
-# objective function
-opt_func<-function(Z,M,R,Theta,D,Sigma1,Sigma2,lambda=1,phi=1,omega=0,Phi1=NULL,Phi2=NULL)
-{
+opt_func<-function(Z,M,R,
+                   Theta,D,
+                   Sigma1,Sigma2,
+                   lambda=1,phi=1,omega=0,
+                   Phi1=NULL,Phi2=NULL) {
   n<-nrow(M)
   k<-ncol(M)
   
-  if(is.null(Phi1)==TRUE)
-  {
-    Phi1<-diag(c(0,rep(1,k)))
-  }
-  if(is.null(Phi2)==TRUE)
-  {
-    Phi2<-diag(rep(1,k+1))
-  }
+  if(is.null(Phi1)==TRUE) { Phi1<-diag(c(0,rep(1,k))) }
+  if(is.null(Phi2)==TRUE) { Phi2<-diag(rep(1,k+1)) }
   
   X<-cbind(Z,M)
   Omega1<-matrix(0,k+1,k+1)
@@ -138,81 +95,48 @@ opt_func<-function(Z,M,R,Theta,D,Sigma1,Sigma2,lambda=1,phi=1,omega=0,Phi1=NULL,
   return(f)
 }
 
-#################################################################
 # ADMM for (adptive) Pathway Lasso without constraint
-mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL,rho=1,rho.increase=FALSE,tol=1e-10,max.itr=10000,thred=1e-10,est_thred=FALSE,Sigma1=NULL,Sigma2=NULL,
-                                trace=FALSE,Theta0=NULL,D0=NULL,alpha0=NULL,beta0=NULL)
-{
+mediation_net_ADMM_NC<-function(Z, M, R,
+                                lambda = 1, omega = 0, phi = 1, # parameters in the adaptive LASSO penalty
+                                rho = 1, rho.increase = FALSE,
+                                tol = 1e-10, max.itr = 10000, thred = 1e-10, 
+                                est_thred = FALSE,
+                                trace=FALSE,
+                                Sigma1=NULL,Sigma2=NULL, # estimate of Sigma1 and Sigma2, if provided, they would be unchanged, otherwise they would be updated together
+                                k = ncol(M),
+                                Phi1 = diag(c(0, rep(1, k))), 
+                                Phi2 = diag(rep(1, k + 1)),
+                                Theta0 = matrix(rep(0, k + 1),nrow = 1),
+                                D0 = matrix(rep(0, k + 1), ncol = 1),
+                                alpha0 = matrix(rep(0, k + 1),nrow = 1),
+                                beta0 = matrix(rep(0, k + 1), ncol = 1)) {
   n<-nrow(M)
-  k<-ncol(M)
-  
-  if(is.null(Phi1)==TRUE)
-  {
-    Phi1<-diag(c(0,rep(1,k)))
-  }
-  if(is.null(Phi2)==TRUE)
-  {
-    Phi2<-diag(rep(1,k+1))
-  }
   
   X<-cbind(Z,M)
   e1<-c(1,rep(0,k))
   
+  sum_Z2 <- sum(Z ** 2)
   # C'
-  C2.hat<-solve(t(Z)%*%Z)%*%t(Z)%*%R
-  tau2.hat<-(t(R-Z%*%C2.hat)%*%(R-Z%*%C2.hat)/n)[1,1]
+  C2.hat <- sum(R * Z) / sum_Z2
+  tau2.hat <- mean((R - Z * C2.hat) ** 2)
   
   # OLS for A
-  A.hat<-solve(t(Z)%*%Z)%*%t(Z)%*%M
-  Theta.tilde<-cbind(1,A.hat)
-  Sigma1.tilde<-diag(diag(t(M-Z%*%A.hat)%*%(M-Z%*%A.hat)/n))
+  A.hat <- t(Z) %*% M / sum_Z2
+  Theta.tilde <- cbind(1,A.hat)
+  E1_hat <- M - Z %*% A.hat
+  Sigma1.tilde <- diag(apply(E1_hat, 2, function(x) mean(x ** 2)))
   
   # Initial values
-  if(is.null(Theta0)==TRUE)
-  {
-    Theta0<-matrix(rep(0,k+1),nrow=1)
-  }
-  if(is.null(alpha0)==TRUE)
-  {
-    alpha0<-matrix(rep(0,k+1),nrow=1)
-  }
-  if(is.null(D0)==TRUE)
-  {
-    D0<-matrix(rep(0,k+1),ncol=1)
-  }
-  if(is.null(beta0)==TRUE)
-  {
-    beta0<-matrix(rep(0,k+1),ncol=1)
-  }
-  if(is.null(Sigma1)==TRUE)
-  {
-    Sigma10<-diag(diag(t(M-Z%*%Theta0[-1])%*%(M-Z%*%Theta0[-1])/n))
-  }else
-  {
-    Sigma10<-Sigma1
-  }
-  if(is.null(Sigma2)==TRUE)
-  {
-    Sigma20<-t(R-X%*%D0)%*%(R-X%*%D0)
-  }else
-  {
-    Sigma20<-Sigma2
-  }
+  Sigma10 <- ifelse(is.null(Sigma1), diag(diag(t(M-Z%*%Theta0[-1])%*%(M-Z%*%Theta0[-1])/n)), Sigma1)
+  Sigma20 <- ifelse(is.null(Sigma2), t(R-X%*%D0)%*%(R-X%*%D0), Sigma2)
   
   nu1=nu2<-rep(0,k+1)
   nu3<-0
   
-  if(rho.increase==FALSE)
-  {
-    rho0<-0
-  }else
-  {
-    rho0<-rho
-  }
+  rho0 <- ifelse(rho.increase, rho, 0)
   
   # Trace
-  if(trace==TRUE)
-  {
+  if(trace==TRUE) {
     Theta.trace=D.trace=alpha.trace=beta.trace<-NULL
     Theta.sg=D.sg=alpha.sg=beta.sg<-NULL
     f<-NULL
@@ -227,8 +151,7 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
   s<-0
   
   time<-system.time(
-    while(diff>=tol&s<=max.itr)
-    {
+    while(diff>=tol&s<=max.itr) {
       s<-s+1
       Omega1<-matrix(0,k+1,k+1)
       Omega1[2:(k+1),2:(k+1)]<-solve(Sigma10)/n
@@ -238,8 +161,7 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
       de.Theta<-solve((t(Z)%*%Z)[1,1]*Omega1+2*rho*diag(rep(1,k+1))+2*rho*e1%*%t(e1))
       nu.Theta<-t(Z)%*%X%*%Omega1-t(nu1)+2*rho*alpha0+(2*rho-nu3)*t(e1)
       Theta.new<-nu.Theta%*%de.Theta
-      if(est_thred==TRUE)
-      {
+      if(est_thred==TRUE) {
         Theta.idx<-which(abs(Theta.new)<thred)
         Theta.new[Theta.idx]<-0
       }
@@ -248,8 +170,7 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
       de.D<-solve(w2[1,1]*t(X)%*%X+2*rho*diag(rep(1,k+1)))
       nu.D<-w2[1,1]*t(X)%*%R-nu2+2*rho*beta0
       D.new<-de.D%*%nu.D
-      if(est_thred==TRUE)
-      {
+      if(est_thred==TRUE) {
         D.idx<-which(abs(D.new)<thred)
         D.new[D.idx]<-0
       }
@@ -258,12 +179,8 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
       alpha.new<-matrix(rep(NA,k+1),nrow=1)
       beta.new<-matrix(rep(NA,k+1),ncol=1)
       J<-c(0,rep(1,k))
-      if(lambda==0)
-      {
-        J[1]<-1
-      }
-      for(j in 1:(k+1))
-      {
+      if(lambda==0) { J[1]<-1 }
+      for(j in 1:(k+1)) {
         pp1<-2*lambda*phi*Phi1[j,j]^2+2*rho
         pp2<-2*lambda*phi*Phi2[j,j]^2+2*rho
         mu1<-nu1[j]+2*rho*Theta.new[1,j]
@@ -288,23 +205,18 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
       alpha0<-alpha.new
       beta0<-beta.new
       
-      if(is.null(Sigma1)==TRUE)
-      {
+      if(is.null(Sigma1)==TRUE) {
         Sigma10<-diag(diag(t(M-Z%*%Theta0[-1])%*%(M-Z%*%Theta0[-1])/n))
-      }else
-      {
+      } else {
         Sigma10<-Sigma1
       }
-      if(is.null(Sigma2)==TRUE)
-      {
+      if(is.null(Sigma2)==TRUE) {
         Sigma20<-t(R-X%*%D0)%*%(R-X%*%D0)
-      }else
-      {
+      } else {
         Sigma20<-Sigma2
       }
       
-      if(trace==TRUE)
-      {
+      if(trace==TRUE) {
         # Trace
         Theta.trace<-rbind(Theta.trace,Theta0)
         D.trace<-cbind(D.trace,D0)
@@ -323,8 +235,7 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
       }
     }
   )
-  if(s>max.itr)
-  {
+  if(s>max.itr) {
     warning("Method does not converge!")
   }
   
@@ -339,19 +250,16 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
   constraint<-cbind(data.frame(t(constraint1)),data.frame(t(constraint2)))
   colnames(constraint)<-c("Satisfied","value")
   
-  if(constraint[1,1]==TRUE)
-  {
+  if(constraint[1,1]==TRUE) {
     A.hat<-matrix(alpha0[-1],nrow=1)
     colnames(A.hat)<-paste0("M",1:k)
     rownames(A.hat)<-"Z"
-  }else
-  {
+  } else {
     A.hat<-matrix(Theta0[-1],nrow=1)
     colnames(A.hat)<-paste0("M",1:k)
     rownames(A.hat)<-"Z"
   }
-  if(constraint[2,1]==TRUE)
-  {
+  if(constraint[2,1]==TRUE) {
     D.hat<-beta0
     C.hat<-matrix(beta0[1],1,1)
     colnames(C.hat)<-"R"
@@ -359,8 +267,7 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
     B.hat<-matrix(beta0[-1],ncol=1)
     colnames(B.hat)<-"R"
     rownames(B.hat)<-paste0("M",1:k) 
-  }else
-  {
+  } else {
     D.hat<-D0
     C.hat<-matrix(D0[1],1,1)
     colnames(C.hat)<-"R"
@@ -381,15 +288,13 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
   d<-sum(abs(c(t(A.hat)*B.hat,C.hat))>thred)
   net.BIC<--2*ll$ll.MR+d*log(n)
   
-  if(trace==TRUE)
-  {
+  if(trace==TRUE) {
     re<-list(lambda=lambda,omega=omega,phi=phi,Phi1=Phi1,Phi2=Phi2,rho=rho,A=A.hat,C=C.hat,B=B.hat,AB=t(A.hat)*B.hat,Sigma=Sigma.hat,
              C2=C2.hat[1,1],tau2=tau2.hat,logLik=data.frame(ZMR=ll$ll.MR,ZR=ll$ll.R,lA=ll$ll.A,lB=ll$ll.B),
              BIC=net.BIC,converge=(s<=max.itr),alpha=alpha0,beta=beta0,Theta.1=Theta0[1,1],constraint=constraint,time=time,
              Theta.trace=Theta.trace,D.trace=D.trace,alpha.trace=alpha.trace,beta.trace=beta.trace,
              Theta.sg=Theta.sg,D.sg=D.sg,alpha.sg=alpha.sg,beta.sg=beta.sg,opt_func.sg=f)
-  }else
-  {
+  } else {
     re<-list(lambda=lambda,omega=omega,phi=phi,Phi1=Phi1,Phi2=Phi2,rho=rho,A=A.hat,C=C.hat,B=B.hat,AB=t(A.hat)*B.hat,Sigma=Sigma.hat,
              C2=C2.hat[1,1],tau2=tau2.hat,logLik=data.frame(ZMR=ll$ll.MR,ZR=ll$ll.R,lA=ll$ll.A,lB=ll$ll.B),
              BIC=net.BIC,converge=(s<=max.itr),alpha=alpha0,beta=beta0,Theta.1=Theta0[1,1],constraint=constraint,time=time)
@@ -397,6 +302,12 @@ mediation_net_ADMM_NC<-function(Z,M,R,lambda=1,omega=0,phi=1,Phi1=NULL,Phi2=NULL
   
   return(re)
 }
+
+
+
+
+
+
 
 
 
